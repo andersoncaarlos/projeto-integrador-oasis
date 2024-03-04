07eed38013e7224ff11ec35c0ba4e818b8e4d757
Anderson <ander_son12@hotmail.com>
2024-03-04 16:28:43 -0300

    update Service usuario

--- src/main/java/com/generation/oasis/service/UsuarioService.java
+++ src/main/java/com/generation/oasis/service/UsuarioService.java
@@ -71,6 +71,7 @@ public class UsuarioService {
 			   usuarioLogin.get().setId(usuario.get().getId());
                 usuarioLogin.get().setNome(usuario.get().getNome());
                 usuarioLogin.get().setFoto(usuario.get().getFoto());
+                usuarioLogin.get().setTipo(usuario.get().getTipo());
                 usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
                 usuarioLogin.get().setSenha("");				
 			   return usuarioLogin;
